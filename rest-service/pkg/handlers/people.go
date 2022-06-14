d84ebb3785387e27f750a65986962b965c84b986
michael <software.engr.full.stack@gmail.com>
2022-06-13 23:37:04 -0700

    rest-service: implement and write tests for "GET /people/:id" handler

--- rest-service/pkg/handlers/people.go
+++ rest-service/pkg/handlers/people.go
@@ -3,13 +3,41 @@ package handlers
 import (
     "net/http"
     "encoding/json"
+    "strings"
     "fmt"
+
+    "github.com/satori/go.uuid"
 
     "github.com/software-engr-full-stack/backend-developer-tests/rest-service/pkg/models"
 )
 
 func People(w http.ResponseWriter, req *http.Request) {
     w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+    id := extractID(req.URL.Path, "/people")
+    if id != "" {
+        person, err := models.FindPersonByID(uuid.Must(uuid.FromString(id)))
+        if err != nil {
+            w.WriteHeader(http.StatusNotFound)
+            msg := jsonError(err)
+            fmt.Fprintln(w, string(msg))
+            return
+        }
+
+        data, err := json.Marshal(person)
+        if err != nil {
+            marshalErrorMsg, err := json.Marshal(map[string]string{"error": "marshaling of person failed"})
+            if err != nil {
+                panic(err)
+            }
+
+            fmt.Fprintln(w, string(marshalErrorMsg))
+            return
+        }
+
+        fmt.Fprintln(w, string(data))
+        return
+    }
 
     data, err := json.Marshal(models.AllPeople())
     if err != nil {
@@ -23,4 +51,21 @@ func People(w http.ResponseWriter, req *http.Request) {
     }
 
     fmt.Fprintln(w, string(data))
+}
+
+func extractID(path, prefix string) string {
+    id := strings.TrimPrefix(path, prefix)
+
+    id = strings.TrimLeft(id, "/")
+
+    return id
+}
+
+func jsonError(err error) []byte {
+    msg, err := json.Marshal(map[string]string{"error": err.Error()})
+    if err != nil {
+        panic(err)
+    }
+
+    return msg
 }
--- rest-service/pkg/handlers/people_test.go
+++ rest-service/pkg/handlers/people_test.go
@@ -76,7 +76,7 @@ func TestPeople(t *testing.T) {
     res := w.Result()
     defer res.Body.Close()
 
-    data := runCommonTests(t, path, res)
+    data := runCommonTests(t, path, res, 200)
 
     tb := titleBuilder{path: path}
 
@@ -105,11 +105,73 @@ func TestPeople(t *testing.T) {
     }
 }
 
-func runCommonTests(t *testing.T, path string, res *http.Response) []byte {
+func TestPeopleID(t *testing.T) {
+    type expectedType struct {
+        person *models.Person
+        statusCode int
+        error
+    }
+    type testType struct {
+        path string
+        expected expectedType
+    }
+
+    expectedPerson := expectedPeople[2]
+    nonExistentID := "d135b79c-ef02-4b1f-81c7-d8c25d423c55"
+    tests := []testType{
+        testType{
+            path: "/people" + "/" + expectedPerson.ID.String(),
+            expected: expectedType{person: expectedPerson, statusCode: 200},
+        },
+
+        testType{
+            path: "/people" + "/" + nonExistentID,
+            expected: expectedType{statusCode: 404, error: fmt.Errorf("user ID %s not found", nonExistentID)},
+        },
+    }
+
+    for _, test := range tests {
+        req := httptest.NewRequest(http.MethodGet, test.path, nil)
+        w := httptest.NewRecorder()
+        People(w, req)
+        res := w.Result()
+        defer res.Body.Close() //nolint:gocritic,deferInLoop
+
+        data := runCommonTests(t, test.path, res, test.expected.statusCode)
+
+        tb := titleBuilder{path: test.path}
+
+        if test.expected.error != nil {
+            var eresp map[string]string
+            title := tb.build("unmarshal response body error")
+            if actual, expected := json.Unmarshal(data, &eresp), error(nil); actual != expected {
+                t.Fatalf("%s: actual not equal to expected, %#v != %#v", title, actual, expected)
+            }
+            title = tb.build("error details")
+            if actual, expected := eresp["error"], test.expected.error.Error(); actual != expected {
+                t.Fatalf("%s: actual not equal to expected, %#v != %#v", title, actual, expected)
+            }
+            continue
+        }
+
+        var actualPerson *models.Person
+        title := tb.build("unmarshal response body error")
+        if actual, expected := json.Unmarshal(data, &actualPerson), error(nil); actual != expected {
+            t.Fatalf("%s: actual not equal to expected, %#v != %#v", title, actual, expected)
+        }
+
+        title = tb.build("person details")
+        if isDeepEqual := reflect.DeepEqual(actualPerson, expectedPerson); !isDeepEqual {
+            t.Fatalf("%s: actual not equal to expected, %#v != %#v", title, actualPerson, expectedPerson)
+        }
+    }
+}
+
+func runCommonTests(t *testing.T, path string, res *http.Response, expectedStatusCode int) []byte {
     tb := titleBuilder{path: path}
 
     title := tb.build("response code")
-    if actual, expected := res.StatusCode, 200; actual != expected {
+    if actual, expected := res.StatusCode, expectedStatusCode; actual != expected {
         t.Fatalf("%s: actual not equal to expected, %#v != %#v", title, actual, expected)
     }
 
