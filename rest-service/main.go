3bbdd2e554d42030b3b54c6608475314ff798daa
michael <software.engr.full.stack@gmail.com>
2022-06-15 20:05:09 -0700

    rest-service: clean up

--- rest-service/main.go
+++ rest-service/main.go
@@ -7,11 +7,6 @@ import (
 
 	"fmt"
 
-	// "strconv"
-	// "github.com/prometheus/client_golang/prometheus"
-	// "github.com/prometheus/client_golang/prometheus/promhttp"
-	// // "github.com/prometheus/client_golang/prometheus/promauto"
-
 	"github.com/software-engr-full-stack/backend-developer-tests/rest-service/pkg/handlers"
 	"github.com/software-engr-full-stack/backend-developer-tests/rest-service/pkg/metrics"
 )
@@ -21,7 +16,6 @@ func main() {
 	fmt.Println()
 
 	var err = metrics.Init()
-	// var err = metricsInit()
 	if err != nil {
 		panic(err)
 	}
@@ -34,7 +28,6 @@ func main() {
 	mux.HandleFunc("/people/", handlers.People)
 
 	mux.HandleFunc("/metrics", metrics.HandleFunc)
-	// mux.HandleFunc("/metrics", metricsHandleFunc)
 
 	port := "8000"
 
@@ -42,7 +35,6 @@ func main() {
 	// For example: "Handler: cors.Handler(middleware.WithLogging(mux))"
     srv := &http.Server{
     	Handler:      metrics.Middleware(mux),
-    	// Handler:      metricsMiddleware(mux),
         Addr:         "0.0.0.0:" + port,
         WriteTimeout: 15 * time.Second,
         ReadTimeout:  15 * time.Second,
@@ -51,80 +43,3 @@ func main() {
     fmt.Println("Server started on PORT " + port)
     log.Fatal(srv.ListenAndServe())
 }
-
-// func metricsHandleFunc(w http.ResponseWriter, r *http.Request) {
-//     promhttp.Handler().ServeHTTP(w, r)
-// }
-
-// type responseWriter struct {
-// 	http.ResponseWriter
-// 	statusCode int
-// }
-
-// func NewResponseWriter(w http.ResponseWriter) *responseWriter {
-// 	return &responseWriter{w, http.StatusOK}
-// }
-
-// func (rw *responseWriter) WriteHeader(code int) {
-// 	rw.statusCode = code
-// 	rw.ResponseWriter.WriteHeader(code)
-// }
-
-// var totalRequests = prometheus.NewCounterVec(
-// 	prometheus.CounterOpts{
-// 		Name: "http_requests_total",
-// 		Help: "Number of get requests.",
-// 	},
-// 	[]string{"path"},
-// )
-
-// var responseStatus = prometheus.NewCounterVec(
-// 	prometheus.CounterOpts{
-// 		Name: "response_status",
-// 		Help: "Status of HTTP response",
-// 	},
-// 	[]string{"status"},
-// )
-
-// var httpDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-// 	Name: "http_response_time_seconds",
-// 	Help: "Duration of HTTP requests.",
-// }, []string{"path"})
-// // var httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
-// // 	Name: "http_response_time_seconds",
-// // 	Help: "Duration of HTTP requests.",
-// // }, []string{"path"})
-
-// func metricsMiddleware(next http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		path := r.URL.Path
-
-// 		timer := prometheus.NewTimer(httpDuration.WithLabelValues(path))
-// 		rw := NewResponseWriter(w)
-// 		next.ServeHTTP(rw, r)
-
-// 		statusCode := rw.statusCode
-
-// 		responseStatus.WithLabelValues(strconv.Itoa(statusCode)).Inc()
-// 		totalRequests.WithLabelValues(path).Inc()
-
-// 		timer.ObserveDuration()
-// 	})
-// }
-
-// func metricsInit() error {
-//     err := prometheus.Register(totalRequests)
-//     if err != nil {
-//         return err
-//     }
-//     err = prometheus.Register(responseStatus)
-//     if err != nil {
-//         return err
-//     }
-//     err = prometheus.Register(httpDuration)
-//     if err != nil {
-//         return err
-//     }
-
-//     return nil
-// }
