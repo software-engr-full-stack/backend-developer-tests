f1d9868c696b85aa2ed28159e4075cd55dd7f411
michael <software.engr.full.stack@gmail.com>
2022-06-22 20:14:06 -0700

    rest-service: change tabs to spaces

--- rest-service/main.go
+++ rest-service/main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-	"net/http"
-	"time"
-	"log"
+    "net/http"
+    "time"
+    "log"
 
-	"fmt"
+    "fmt"
 
-	"github.com/software-engr-full-stack/backend-developer-tests/rest-service/pkg/handlers"
-	"github.com/software-engr-full-stack/backend-developer-tests/rest-service/pkg/metrics"
+    "github.com/software-engr-full-stack/backend-developer-tests/rest-service/pkg/handlers"
+    "github.com/software-engr-full-stack/backend-developer-tests/rest-service/pkg/metrics"
 )
 
 func main() {
-	fmt.Println("SP// Backend Developer Test - RESTful Service")
-	fmt.Println()
+    fmt.Println("SP// Backend Developer Test - RESTful Service")
+    fmt.Println()
 
-	var err = metrics.Init()
-	if err != nil {
-		panic(err)
-	}
+    var err = metrics.Init()
+    if err != nil {
+        panic(err)
+    }
 
-	mux := http.NewServeMux()
+    mux := http.NewServeMux()
 
-	// Notes: I wanted to go for the minimal approach hence this ugly hack.
-	// Using tools like gorilla mux will make these 2 lines look a lot better.
-	mux.HandleFunc("/people", handlers.People)
-	mux.HandleFunc("/people/", handlers.People)
+    // Notes: I wanted to go for the minimal approach hence this ugly hack.
+    // Using tools like gorilla mux will make these 2 lines look a lot better.
+    mux.HandleFunc("/people", handlers.People)
+    mux.HandleFunc("/people/", handlers.People)
 
-	mux.HandleFunc("/metrics", metrics.HandleFunc)
+    mux.HandleFunc("/metrics", metrics.HandleFunc)
 
-	port := "8000"
+    port := "8000"
 
-	// Notes: written this way so I can easily add middleware in the future.
-	// For example: "Handler: cors.Handler(middleware.WithLogging(mux))"
+    // Notes: written this way so I can easily add middleware in the future.
+    // For example: "Handler: cors.Handler(middleware.WithLogging(mux))"
     srv := &http.Server{
-    	Handler:      metrics.Middleware(mux),
+        Handler:      metrics.Middleware(mux),
         Addr:         "0.0.0.0:" + port,
         WriteTimeout: 15 * time.Second,
         ReadTimeout:  15 * time.Second,
